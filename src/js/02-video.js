2c553ac80399ea8d8d03b554e8b3ce66e83aca8f
Illia Kazmin <kazmin1c@gmail.com>
2022-08-31 12:54:26 +0200

    add second part of HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+};
+
+refs.formEl.addEventListener('input', throttle(onFormClick, 500));
+
+function onFormClick(evt) {
+  console.log(evt.target.value);
+}
