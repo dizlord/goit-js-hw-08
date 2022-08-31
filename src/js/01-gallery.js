30e202809ac5ae00f4c0b01c93632555be1454d5
Illia Kazmin <kazmin1c@gmail.com>
2022-08-31 12:02:50 +0200

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+function createGallery(galleryItemsPar) {
+  const galleryString = galleryItemsPar
+    .map(galleryItem => {
+      return `
+          <a class="gallery__item" href="${galleryItem.original}">
+            <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+          </a>
+      `;
+    })
+    .join('');
+  refs.galleryEl.insertAdjacentHTML('afterbegin', galleryString);
+}
+
+createGallery(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  nav: true,
+  close: true,
+  caption: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
